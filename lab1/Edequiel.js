8733ec23b32c82d74bc962f9ea82c017451179b1
eromanmg <59655509+eromanmg@users.noreply.github.com>
2021-01-21 13:57:55 -0500

    new changes

---
+++ lab1/Edequiel.js
@@ -0,0 +1,10 @@
+const Person = require("./Person.js");
+
+class Edequiel extends Person {
+    constructor(name, colors) {
+        super(name, colors)
+
+    }
+}
+
+module.exports = Edequiel;
---
+++ lab1/index.js
@@ -0,0 +1,7 @@
+const Edequiel = require("./Edequiel.js");
+
+let p = new Edequiel ("Edequiel", [
+    "FF0000"
+]);
+
+p.speak();
