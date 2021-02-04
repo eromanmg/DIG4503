347fc5e378454ffa3988772c0c9be8080320fc30
eromanmg <59655509+eromanmg@users.noreply.github.com>
2021-02-04 10:03:59 -0500

    changes

---
+++ lab3/lab3app/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import HomePage  from './homepage'
+
+function App() {
+    const fname = 'Edequiel';
+    return(
+        <HomePage fristname={fname} />
+    )
+}
+
+export default App;
