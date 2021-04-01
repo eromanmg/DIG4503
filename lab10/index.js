f221dab476a8c7de64bef1abc6dbca1408980d69
eromanmg <59655509+eromanmg@users.noreply.github.com>
2021-04-01 02:19:26 -0400

    changes

---
+++ lab10/index.js
@@ -0,0 +1,54 @@
+import Express from "express";
+import fs from "fs";
+import cors from "cors";
+
+const App = Express();
+const port = 3010;
+App.use(cors());
+
+let fileContents = fs.readFileSync("database.json");
+
+let database = JSON.parse(fileContents);
+
+App.use("/", Express.static("client/build"));
+
+App.get("/employees/:name", (req, res) => {
+    let result = {"error": "Not found"};
+
+    database.forEach((value) => {//digs into the database
+        if(req.params.name == value.name) {//if data requested is equal == shows result
+            result = value;
+        }
+    });
+
+    res.json(result);//gives result
+})
+
+App.get("/ages/:number", (req, res) => {
+    let result = {"error": "Not found"};
+
+    database.forEach((value) => {
+        if(req.params.ages == value.ages) {
+            result = value;
+        }
+    });
+
+    res.json(result);
+})
+
+App.post("/list/:name/:age/", (req, res) => {
+    let result = {
+        "name": req.params.name,
+        "age": parseInt(req.params.age)
+    };
+
+    database.push(result)
+
+    fs.writeFileSync("database.json", JSON.stringify(database, null, '\t'));
+
+    res.json(result);
+});
+
+App.listen(port, () => {
+    console.log("Server running!");
+})
