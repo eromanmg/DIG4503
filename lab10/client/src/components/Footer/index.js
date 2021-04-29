c6d72464c729937054c72afd1fab849a27d00f45
eromanmg <59655509+eromanmg@users.noreply.github.com>
2021-04-29 09:35:14 -0400

    changes

---
+++ lab10/client/src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../footerstyling.css';
+
+function Footer () {
+    return(
+        <div className="f">
+            <footer>
+                <p>Follow us on our social media</p>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer;
